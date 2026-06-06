9a0c7ddd8a6c301572b675c34843eb695f035c24
Firdose Shaik <shaikfirdose6652+github@gmail.com>
2026-06-06 22:49:21 +0530

    project added

---
+++ tea-stall-management-system/src/main/java/com/tsms/tea_stall_management_system/TeaStallManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.tsms.tea_stall_management_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeaStallManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeaStallManagementSystemApplication.class, args);
+	}
+
+}
---
+++ tea-stall-management-system/src/test/java/com/tsms/tea_stall_management_system/TeaStallManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tsms.tea_stall_management_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeaStallManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
